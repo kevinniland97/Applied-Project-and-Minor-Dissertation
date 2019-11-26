4dbc26b605e2025f44cd3c410ecf13360db82996
Kevin Niland <G00342279@gmit.ie>
2019-11-26 22:04:41 +0000

    Grid started - needs more work

---
+++ fyp_app/src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Node.css';
+
+export default class Node extends Component {
+    render() {
+        const {
+            column, row,
+            isFinish, isStart, isWall,
+            onMouseDown, onMouseEnter, onMouseUp,
+        } = this.properties;
+
+        const extraClassName =
+            isFinish ? 'node-finish'
+                : isStart ? 'node-start'
+                    : isWall ? 'node-wall'
+                        : '';
+
+        return (
+            <div id={`node-${row}-${column}`}
+                className={`node ${extraClassName}`}
+                onMouseDown={() => onMouseDown(row, column)}
+                onMouseEnter={() => onMouseEnter(row, column)}
+                onMouseUp={() => onMouseUp()}></div>
+        );
+    }
+}
