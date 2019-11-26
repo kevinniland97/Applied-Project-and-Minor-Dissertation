0ff7fde89d91682f6e4e3207c360fe9aaecd9674
Kevin Niland <G00342279@gmit.ie>
2019-11-26 21:54:35 +0000

    Dijkstra algorithm finished in JavaScript. May need improving/fixing

--- fyp_app/backend/algorithms/dijkstra.js
+++ fyp_app/backend/algorithms/dijkstra.js
@@ -1,10 +1,63 @@
 // Dijkstra' algorithm written in JavaScript
-function sortNodes(unvisitedNodes) {
+function sortNodesByDistance(unvisitedNodes) {
     unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
 
-function updateUnvisitedNeighbors(node, grid) {
+function updateUnvisitedNeighbours(node, grid) {
+    const unvisitedNeighbours = getUnvisitedNeighbours(node, grid);
+
+    for (const neighbour of unvisitedNeighbours) {
+        neighbour.distance = node.distance + 1;
+        neighbour.previousNode = node;
+    }
+}
+
+function getUnvisitedNeighbours(node, grid) {
+    const neighbours = [];
+    const {column, row} = node;
+
+    if (row > 0) {
+        neighbours.push(grid[row - 1][column]);
+    }
+
+    if (row < grid.length - 1) {
+        neighbours.push(grid[row + 1][column]);
+    }
+
+    if (column > 0) {
+        neighbours.push(grid[row][column - 1]);
+    }
+
+    if (column < grid[0].length - 1) {
+        neighbours.push(grid[row][column + 1]);
+    }
+
+    return neighbours.filter(neighbour => !neighbour.isVisited);
+}
+
+function getAllNodes(grid) {
+    const nodes = [];
+
+    for (const row of grid) {
+        for (const node of row) {
+            nodes.push(node);
+        }
+    }
+
+    return nodes;
+}
+
+// Backtracks from the finishNode to find the shortest path. Only works when called *after* the dijkstra method above.
+export function getNodesInShortestPathOrder(finishNode) {
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
     
+    while (currentNode !== null) {
+        nodesInShortestPathOrder.unshift(currentNode);
+        currentNode = currentNode.previousNode;
+    }
+    
+    return nodesInShortestPathOrder;
 }
 
 export function dijkstra(grid, startNode, finishNode) {
