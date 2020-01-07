3292ad12d135da43c7dbbcb5dd5a2db1638ba813
Kevin Niland <G00342279@gmit.ie>
2020-01-07 15:20:16 +0000

    Server side started

--- fyp_app/src/backend/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-
-server.listen(9999);
--- fyp_app/src/index.js
+++ fyp_app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import PathfindingHome from './PathfindingHome';
-import SortingHome from './SortingHome'
+import PathfindingHome from '../src/PathfindingHome';
 
 ReactDOM.render(<PathfindingHome />, document.getElementById('root'));
-ReactDOM.render(<SortingHome />, document.getElementById('root'));
