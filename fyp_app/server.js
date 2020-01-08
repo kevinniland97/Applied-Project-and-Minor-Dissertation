1e3164d777d696874c361375ca7e18f5ced34982
Kevin Niland <G00342279@gmit.ie>
2020-01-08 14:52:08 +0000

    index.html started - will be main page from which the application runs from

--- fyp_app/server.js
+++ fyp_app/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 // app.use("/public", express.static(__dirname + "/public"))
+console.log("\nInitializing server...");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
 app.listen(1337, () => {
-  console.log("Server running...");
+  console.log("Server running");
 });
