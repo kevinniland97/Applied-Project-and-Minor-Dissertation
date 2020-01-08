c4bb2aaba4bd36796faf1f88556067a7f9072805
Kevin Niland <G00342279@gmit.ie>
2020-01-08 16:29:21 +0000

    Database side started - able to capture screenshot of webpage

--- fyp_app/server.js
+++ fyp_app/server.js
@@ -4,7 +4,7 @@ const app = express();
 // app.use("/public", express.static(__dirname + "/public"))
 console.log("\nInitializing server...");
 
-app.get("/", (req, res) => {
+app.get("/algorithms_visualizer", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
