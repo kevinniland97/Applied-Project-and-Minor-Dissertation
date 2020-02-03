05de3a1cafb390c467161fc311b09faebfd6a1cc
Kevin Niland <G00342279@gmit.ie>
2020-02-03 18:11:23 +0000

    Insertion sort logic started

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Routes from './components/Routes.js';
+import Routes from '../components/Routes.js';
 import { BrowserRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import 'semantic-ui-css/semantic.min.css';
