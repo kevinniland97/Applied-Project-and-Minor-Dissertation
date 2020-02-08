dba74b03159b47f62db0789a1c0648fccdd54063
Kevin Niland <G00342279@gmit.ie>
2020-02-08 20:08:06 +0000

    Selection sort logic started

--- src/algorithms/SelectionSort.js
+++ src/algorithms/SelectionSort.js
@@ -1,3 +1,29 @@
 export default class SelectionSort {
-    
+    static selectionSort(array, sortHistory, highlightHistory) {
+        this.clearArray(sortHistory);
+        this.clearArray(highlightHistory);
+
+        let leftColumn = -1;
+
+        for (let i = 0; i < array.length; i++) {
+            let minimum = i;
+            leftColumn = min;
+
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minimum]) {
+                    minimum = j;
+                }
+
+                sortHistory.push(array.slice());
+                highlightHistory.push([j, leftColumn, minimum]);
+            }
+
+            if (i !== minimum) {
+                this.swap(array, i, minimum);
+            }
+        }
+
+        sortHistory.push(array.slice());
+        highlightHistory.push([-1, array.length - 1]);
+    }
 }
