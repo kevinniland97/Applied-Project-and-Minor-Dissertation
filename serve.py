0bda9d09b9ef277a4a07c916e7bb63193f174225
Kevin Niland <G00342279@gmit.ie>
2020-02-14 15:38:29 +0000

    Register function started

--- serve.py
+++ serve.py
@@ -10,3 +10,19 @@ app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'algorithms_visualizer'
 app.config['MONGO_URI'] = 'mongodb://admin:admin123@ds217809.mlab.com:17809/algorithms_visualizer?retryWrites=false'
+app.config['JWT_SECRET_KEY'] = 'secret'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+CORS(app)
+
+@app.route(methods=['POST'])
+def register():
+    users = mongo.db.users
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['first_name']
+    email = request.get_json()['email']
+    password = bcrypt.generate_password_hash(request.get_json()['password'].decode('utf-8'))
+    created = datetime.utcnow()
