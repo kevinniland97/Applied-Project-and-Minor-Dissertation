49e2f3d1d9e12e6e87c6c66d73f3644ef2cd4808
Kevin Niland <G00342279@gmit.ie>
2020-02-26 20:57:13 +0000

    Work started on Quick Sort

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -1,3 +1,27 @@
 export default class QuickSort {
-    
+
+    /**
+     * Swaps items in the array depending on their size
+     * 
+     * @param {*} array - The array of items to be sorted
+     * @param {*} i - Index
+     * @param {*} j - Index
+     */
+    static swap(array, i, j) {
+        let temp = array[i];
+
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    /**
+     * Clear the array i.e. pop an item off the array
+     * 
+     * @param {*} array - The array of items
+     */
+    static clearArray(array) {
+        while (array.length) {
+            array.pop();
+        }
+    }
 }
