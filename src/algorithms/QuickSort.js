af87b9251c79dd90bc23bb5e78cf668155b728c5
Kevin Niland <G00342279@gmit.ie>
2020-02-29 15:12:12 +0000

    More work on Quick Sort

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -9,21 +9,47 @@ export default class QuickSort {
     static quickSortIterative(array, l, h, sortHistory, highlightHistory) {
         let stack = [];
 
-        let top = -1; 
-  
-        stack[++top] = l; 
-        stack[++top] = h; 
-  
-        while (top >= 0) { 
-            h = stack[top--]; 
-            l = stack[top--]; 
-    
+        let top = -1;
+
+        stack[++top] = l;
+        stack[++top] = h;
+
+        while (top >= 0) {
+            h = stack[top--];
+            l = stack[top--];
+
             let p = this.partition(array, l, h, sortHistory, highlightHistory);
+
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
         }
     }
 
-    static partition(array, l, h, sortHistory, highlightHistory) { 
+    static partition(array, l, h, sortHistory, highlightHistory) {
+        let x = array[h];
+        let i = (l - 1);
 
+        for (let j = l; j <= h - 1; j++) {
+            sortHistory.push(array.slice());
+            highlightHistory.push([j, l, h]);
+
+            if (array[j] <= x) {
+                i++;
+
+                this.swap(array, i, j);
+            }
+        }
+
+        this.swap(array, i + 1, h);
+
+        return (i + 1);
     }
 
     /**
