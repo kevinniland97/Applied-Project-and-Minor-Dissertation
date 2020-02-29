f03a41aad2295e748197085a6b387c76f6b9f842
Kevin Niland <G00342279@gmit.ie>
2020-02-29 18:44:19 +0000

    Started merge sort

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -1,3 +1,9 @@
 export default class MergeSort {
+    static mergeSort(array, sortHistory, highlightHistory) {
+
+    }
     
+    static mergeBottomUp(array, left, step, sortHistory, highlightHistory) {
+
+    }
 }
