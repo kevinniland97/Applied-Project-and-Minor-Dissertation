12fbd712f6f311226c75c01357c400907fd6761a
Kevin Niland <G00342279@gmit.ie>
2020-02-29 19:29:53 +0000

    Fixed bug with merge sort

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -41,30 +41,29 @@ export default class MergeSort {
         sortHistory.push(array.slice());
         highlightHistory.push([-1]);
     }
-}
 
-/**
- * Swaps items in the array depending on their size
- * 
- * @param {*} array - The array of items to be sorted
- * @param {*} i - Index
- * @param {*} j - Index
- */
-static swap(array, i, j) {
-    let temp = array[i];
+    /**
+     * Swaps items in the array depending on their size
+     * 
+     * @param {*} array - The array of items to be sorted
+     * @param {*} i - Index
+     * @param {*} j - Index
+     */
+    static swap(array, i, j) {
+        let temp = array[i];
 
-    array[i] = array[j];
-    array[j] = temp;
-}
-
-/**
- * Clear the array i.e. pop an item off the array
- * 
- * @param {*} array - The array of items
- */
-static clearArray(array) {
-    while (array.length) {
-        array.pop();
+        array[i] = array[j];
+        array[j] = temp;
     }
-}
+
+    /**
+     * Clear the array i.e. pop an item off the array
+     * 
+     * @param {*} array - The array of items
+     */
+    static clearArray(array) {
+        while (array.length) {
+            array.pop();
+        }
+    }
 }
