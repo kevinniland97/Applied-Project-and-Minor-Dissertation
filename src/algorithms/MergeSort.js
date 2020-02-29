1370d90f62add9b6df0252bc3c549fd75b3f3890
Kevin Niland <G00342279@gmit.ie>
2020-02-29 18:55:35 +0000

    More work on merge sort

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -1,9 +1,46 @@
 export default class MergeSort {
     static mergeSort(array, sortHistory, highlightHistory) {
+        let step = 1;
 
+        while (step < array.length) {
+            let left = 0;
+
+            while (left + step < array.length) {
+                this.mergeBottomUp(array, left, step, sortHistory, highlightHistory);
+                
+                left += step * 2;
+            }
+
+            step *= 2;
+        }
     }
-    
+
     static mergeBottomUp(array, left, step, sortHistory, highlightHistory) {
 
+    }
+
+    /**
+     * Swaps items in the array depending on their size
+     * 
+     * @param {*} array - The array of items to be sorted
+     * @param {*} i - Index
+     * @param {*} j - Index
+     */
+    static swap(array, i, j) {
+        let temp = array[i];
+
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    /**
+     * Clear the array i.e. pop an item off the array
+     * 
+     * @param {*} array - The array of items
+     */
+    static clearArray(array) {
+        while (array.length) {
+            array.pop();
+        }
     }
 }
